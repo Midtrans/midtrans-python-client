89bf4064dc59c94bd749c97ef00a2324034e6b74
Rizda Dwi Prasetya <rizda.prasetya@midtrans.com>
2018-10-24 19:20:02 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pkg_req = [
+    'requests>=2.3.0'
+]
+test_req = pkg_req + [
+    'pytest>=3.0.6'
+]
+
+setuptools.setup(
+    name="midtransclient",
+    version="1.0.0",
+    author="Rizda Prasetya",
+    author_email="rizda.prasetya@midtrans.com",
+    license='MIT',
+    description="Official Midtrans Payment API Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/midtrans/midtrans-client-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=pkg_req,
+    tests_requires=test_req
+)
