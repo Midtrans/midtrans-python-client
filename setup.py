51f4cbed979b9408a3cbdcebd40329cf777137dc
Rizda Dwi Prasetya <rizda.prasetya@midtrans.com>
2019-06-24 09:33:49 +0000

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_req = pkg_req + [
 
 setuptools.setup(
     name="midtransclient",
-    version="1.0.7",
+    version="1.1.0",
     author="Rizda Prasetya",
     author_email="rizda.prasetya@midtrans.com",
     license='MIT',
